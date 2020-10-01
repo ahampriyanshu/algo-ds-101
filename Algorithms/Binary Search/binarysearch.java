eced82b375d3c7ff03b64dabfc8eb272c5d4ebe0
Restyled.io <commits@restyled.io>
2020-10-01 17:04:59 +0000

    Restyled by clang-format

--- Algorithms/Binary Search/binarysearch.java
+++ Algorithms/Binary Search/binarysearch.java
@@ -1,20 +1,22 @@
 class Solution {
 
-    public int BinarySearch(int[] nums, int target) {
+  public int BinarySearch(int[] nums, int target) {
 
-      int left = 0;
-      int right = nums.length - 1;
+    int left = 0;
+    int right = nums.length - 1;
 
-      while (left <= right) {
-        int pivot = left + (right - left) / 2;
+    while (left <= right) {
+      int pivot = left + (right - left) / 2;
 
-        if (nums[pivot] == target) return pivot;
+      if (nums[pivot] == target)
+        return pivot;
 
-        if (target < nums[pivot]) right = pivot - 1;
-        
-        else left = pivot + 1;
-      }
-      return -1;
+      if (target < nums[pivot])
+        right = pivot - 1;
+
+      else
+        left = pivot + 1;
     }
-
+    return -1;
   }
+}
