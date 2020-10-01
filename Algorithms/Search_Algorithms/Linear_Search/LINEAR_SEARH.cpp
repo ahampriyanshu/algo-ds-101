032484524174126204559f257c8c929fa87eae28
Restyled.io <commits@restyled.io>
2020-10-01 16:45:29 +0000

    Restyled by astyle

--- Algorithms/Search_Algorithms/Linear_Search/LINEAR_SEARH.cpp
+++ Algorithms/Search_Algorithms/Linear_Search/LINEAR_SEARH.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int main() 
+int main()
 {
     cout<<"Enter The Size Of Array:   ";
     int size;
@@ -11,29 +11,29 @@ int main()
     int array[size], key,i;
 
     // Taking Input In Array
-     for(int j=0;j<size;j++)
+    for(int j=0; j<size; j++)
     {
         cout<<"Enter "<<j<<" Element: ";
         cin>>array[j];
     }
 
-     //Your Entered Array Is
-    for(int a=0;a<size;a++)
+    //Your Entered Array Is
+    for(int a=0; a<size; a++)
     {
-       cout<<"array[ "<<a<<" ]  =  ";
-       cout<<array[a]<<endl;
+        cout<<"array[ "<<a<<" ]  =  ";
+        cout<<array[a]<<endl;
     }
 
-     cout<<"Enter Key To Search  in Array";
-     cin>>key;
+    cout<<"Enter Key To Search  in Array";
+    cin>>key;
 
-    for(i=0;i<size;i++)
-   {
-      if(key==array[i])
-      {
-        cout<<"Key Found At Index Number :  "<<i<<endl;
-        break;
-       }
+    for(i=0; i<size; i++)
+    {
+        if(key==array[i])
+        {
+            cout<<"Key Found At Index Number :  "<<i<<endl;
+            break;
+        }
     }
 
 
@@ -45,5 +45,5 @@ int main()
     {
         cout<<"KEY NOT FOUND in Array  ";
     }
-   return 0;
+    return 0;
 }
