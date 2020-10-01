9b282b05b17b4e9c37ea7b495f4c630b4176d576
Restyled.io <commits@restyled.io>
2020-10-01 17:44:12 +0000

    Restyled by black

--- Algorithms/Sort_Algorithms/Bubble_Sort/Bubble_Sort.py
+++ Algorithms/Sort_Algorithms/Bubble_Sort/Bubble_Sort.py
@@ -6,11 +6,13 @@
 array = [5, 4, 3, 2, 1, 6]
 
 for i in range(0, len(array)):
-    for j in range(0, len(array)-1):
-        if(array[j] > array[j+1]):  # swaps if greater element is before the smaller one
+    for j in range(0, len(array) - 1):
+        if (
+            array[j] > array[j + 1]
+        ):  # swaps if greater element is before the smaller one
             temp = array[j]
-            array[j] = array[j+1]
-            array[j+1] = temp
+            array[j] = array[j + 1]
+            array[j + 1] = temp
 
 
 print(array)
