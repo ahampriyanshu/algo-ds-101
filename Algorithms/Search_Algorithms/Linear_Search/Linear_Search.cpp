7dcb5be7785ab42f3328db57a86030f6557f5163
Restyled.io <commits@restyled.io>
2020-10-01 16:22:35 +0000

    Restyled by clang-format

--- Algorithms/Search_Algorithms/Linear_Search/Linear_Search.cpp
+++ Algorithms/Search_Algorithms/Linear_Search/Linear_Search.cpp
@@ -1,20 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int n;
-    cin>>n;						//size of the array
-    int element;
-    cin>>element;		//element that have to searched
-    int arr[n];
-    for(int i=0; i<n; i++) {
-        cin>>arr[i];					//taking array input
+  int n;
+  cin >> n; // size of the array
+  int element;
+  cin >> element; // element that have to searched
+  int arr[n];
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i]; // taking array input
+  }
+  for (int i = 0; i < n; i++) {
+    if (arr[i] == element) {
+      cout << "YES" << endl; // doing linearly searching and prints "YES" if
+                             // found else "NO"
+      return 0;
     }
-    for(int i=0; i<n; i++) {
-        if(arr[i]==element) {
-            cout<<"YES"<<endl;			//doing linearly searching and prints "YES" if found else "NO"
-            return 0;
-        }
-    }
-    cout<<"NO"<<endl;
+  }
+  cout << "NO" << endl;
 }
