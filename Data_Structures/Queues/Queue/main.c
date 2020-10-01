006f58ddf7334571f71a27a2c0a84d178c18c14e
Restyled.io <commits@restyled.io>
2020-10-01 18:17:03 +0000

    Restyled by clang-format

--- Data_Structures/Queues/Queue/main.c
+++ Data_Structures/Queues/Queue/main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+int main() {
+  printf("Hello world!\n");
+  return 0;
 }
--- Data_Structures/Tower_of_Hanoi.c
+++ Data_Structures/Tower_of_Hanoi.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-void TOH(int n,char A,char B,char C) {
-    if(n>=1) {
-        TOH(n-1,A,C,B);
-        printf("%c to %c\n",A,C);
-        TOH(n-1,B,A,C);
-    }
+void TOH(int n, char A, char B, char C) {
+  if (n >= 1) {
+    TOH(n - 1, A, C, B);
+    printf("%c to %c\n", A, C);
+    TOH(n - 1, B, A, C);
+  }
 }
-int main()
-{
-    int n;
-    printf("Enter the number of disks: ");
-    scanf("%d",&n);
-    TOH(n,'A','B','C');
-    return 0;
+int main() {
+  int n;
+  printf("Enter the number of disks: ");
+  scanf("%d", &n);
+  TOH(n, 'A', 'B', 'C');
+  return 0;
 }
-
