75e1eb3a30379fec2c4a16d343192336daaffe0d
Nirali0029 <58932139+Nirali0029@users.noreply.github.com>
2020-10-02 23:10:19 +0530

    Create anagram.py

---
+++ Algorithms/String_Algorithms/anagram.py
@@ -0,0 +1,9 @@
+def check(s1, s2): 
+    if(sorted(s1)== sorted(s2)): 
+        print("The strings are anagrams.")  
+    else: 
+        print("The strings are not anagrams.")          
+            
+s1 ="listen"
+s2 ="silent" 
+check(s1, s2)
