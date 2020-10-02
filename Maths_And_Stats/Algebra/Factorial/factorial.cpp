cc9248c7fb7725949e8c2f2810a27d0620e2acb8
SubhayanDas08 <nips.subhayan@gmail.com>
2020-10-03 01:23:00 +0530

    Added factorial.cpp in Maths_And_Stats => Algebra => Factorial => factorial.cpp

---
+++ Maths_And_Stats/Algebra/Factorial/factorial.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int factorial(int n)
+	{
+	
+	if(n==0||n==1)
+		return 1;
+		
+	return(n*factorial(n-1));
+	}
+
+int main()
+{
+int num;
+
+cout<<"Enter the number of which want factorial: ";
+cin>>num;
+
+
+cout<<"The factorial is: "<<factorial(num);
+
+}
