5b5443ccdf8dd6ec11ad2d7d98eaaba235299ac4
Fabian <diadochokinetic@gmail.com>
2020-10-03 14:39:52 +0200

    Added Maths_And_Stats => Geometry => Scalar_Product => Scalar_Product.py

---
+++ Maths_And_Stats/Geometry/Scalar_Product/Scalar_Product.py
@@ -0,0 +1,10 @@
+"""
+LANGUAGE: PYTHON
+AUTHOR: Diadochokinetic
+GITHUB: https://github.com/Diadochokinetic
+"""
+
+from operator import mul
+
+def Scalar_Product(x, y):
+    return sum(map(mul, x, y))
