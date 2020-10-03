28bc2e522a094bdfa50e0a9573505169bc9768f6
Pulkit Verma <55025041+verma-pulkit@users.noreply.github.com>
2020-10-03 21:06:17 +0530

    Took input

--- Maths_And_Stats/Geometry/Vector_Product/Vector_Product.cpp
+++ Maths_And_Stats/Geometry/Vector_Product/Vector_Product.cpp
@@ -13,10 +13,16 @@ vector <int> vector_product (vector <int>a, vector <int> b){
 	return ans;
 }
 
-//int main(){
-	//vector <int> a = {1,2};
-	//vector <int> b = {4,5,6};
-	//for (auto x:vector_product(a,b))
-		//cout<<x<<" ";
-	//return 0;
-//}
+int main(){
+	vector <int> a (3);
+	vector <int> b (3);
+	cout<<"Enter the coordinartes of First Vector"<<endl;
+	cin>>a[0]>>a[1]>>a[2];
+	cout<<"Enter the coordinartes of First Vector"<<endl;
+	cin>>b[0]>>b[1]>>b[2];
+	cout<<"Output is :"
+	for (auto x:vector_product(a,b))
+		cout<<x<<" ";
+	cout<<endl;
+	return 0;
+}
