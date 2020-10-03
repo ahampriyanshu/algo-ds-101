06ff31ea5200956b14da041067653cb1671d383d
Fabian <diadochokinetic@gmail.com>
2020-10-03 14:49:32 +0200

    Added Maths_And_Stats => Geometry => Distance_Formula => Distance_Formula.py

---
+++ Maths_And_Stats/Geometry/Distance_Formula/Distance_Formula.py
@@ -0,0 +1,11 @@
+"""
+LANGUAGE: PYTHON
+AUTHOR: Diadochokinetic
+GITHUB: https://github.com/Diadochokinetic
+"""
+
+from math import sqrt
+
+def Distance_Formula(x, y):
+    return sqrt((y[0]-x[0])**2+(y[1]-x[1])**2)
+
