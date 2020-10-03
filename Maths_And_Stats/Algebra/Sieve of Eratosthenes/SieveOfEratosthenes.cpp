3481d1621972409abe4b3b59a85276b8b5bfe6eb
chinmaym07 <b418020@iiit-bh.ac.in>
2020-10-04 01:43:18 +0530

    Updated SieveOfEratosthenes.cpp

--- Maths_And_Stats/Algebra/Sieve of Eratosthenes/SieveOfEratosthenes.cpp
+++ Maths_And_Stats/Algebra/Sieve of Eratosthenes/SieveOfEratosthenes.cpp
@@ -7,36 +7,28 @@ using namespace std;
 
 void Sieve(int n) 
 { 
-    // Create a boolean array "prime[0..n]" and initialize 
-    // all entries it as true. A value in prime[i] will 
-    // finally be false if i is Not a prime, else true. 
     bool prime[n+1]; 
     memset(prime, true, sizeof(prime)); 
   
     for (int p=2; p*p<=n; p++) 
     { 
-        // If prime[p] is not changed, then it is a prime 
         if (prime[p] == true) 
         { 
-            // Update all multiples of p greater than or  
-            // equal to the square of it 
-            // numbers which are multiple of p and are 
-            // less than p^2 are already been marked.  
+            
             for (int i=p*p; i<=n; i += p) 
                 prime[i] = false; 
         } 
     } 
     int c = 0;
-    // Counting all prime numbers <= n; 
+  
     for (int p=2; p<=n; p++) 
     {
         if (prime[p])
             c++;
     }
 
-    cout << "Prime Count :- " << c << "\n";
-} 
-// Driver code 
+    cout << "Prime Numbers <= n:- " << c << "\n";
+}
 int main() 
 { 
     int N;
