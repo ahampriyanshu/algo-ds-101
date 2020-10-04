d3831413fc55e54b3027963fbca06ca86d1309a3
sakshi kumari <56168294+sakshi-06@users.noreply.github.com>
2020-10-05 01:55:28 +0530

    added c++ beat_sort file

--- Algorithms/String_Algorithms/Palindrome.java
+++ Algorithms/String_Algorithms/Palindrome.java
@@ -1,27 +1,20 @@
-/**
- * Compile and execute: java -classpath Palindrome.jar Palindrome.java
- *
- * @author  Ytalo Ramon
- */
+public class palindrome {
+  static boolean isPalindrome(String word) {
+    int length = word.length();
 
-package String_Algorithms;
-
-public class Palindrome {
-
-    public static void main(String[] args) {
-
-        String inputs[] = { "ana", "carl", "anaa", "level", "levell", "a", "deified", "deiffied", "civicc", "tesett",
-                "vuc", "amaamaamaama" };
-
-        for (String value : inputs)
-            System.out.println("\"" + value + "\" is palindrome? " + (is_palindrome(value) ? "Yes" : "No"));
-
+    for(int i = 0; i < length/2; i++)
+    {
+        if(word.charAt(i) != word.charAt(length - 1 - i))
+        {
+            return false;
+        }
     }
 
-    static boolean is_palindrome(String value) {
-        int i = -1, j = value.length();
-        while (i < j && value.charAt(++i) == value.charAt(--j));
+    return true;
+  }
 
-        return j <= i;
-    }
+  public static void main(String[] args) {
+    System.out.println(isPalindrome("Hello"));
+    System.out.println(isPalindrome("Kayak"));
+  }
 }
