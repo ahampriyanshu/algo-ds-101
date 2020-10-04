b66f65738c75dc4d66547008a0034d5080a8611b
Misha Kushka <kushka.misha@gmail.com>
2020-10-04 09:49:39 +0300

    Add consts

--- Algorithms/Dynamic_Programming/Gauss_Easter_Date/easterDate.js
+++ Algorithms/Dynamic_Programming/Gauss_Easter_Date/easterDate.js
@@ -8,8 +8,8 @@ const rl = readline.createInterface({
 rl.question('\nFor which year do you want to know Easter date?\n> ', year => {
   year = parseInt(year)
 
-  a = (19 * (year % 19) + 15) % 30
-  b = (2 * (year % 4) + 4 * (year % 7) + 6 * a + 6) % 7
+  const a = (19 * (year % 19) + 15) % 30
+  const b = (2 * (year % 4) + 4 * (year % 7) + 6 * a + 6) % 7
 
   console.log(`Easter date in ${year} is \
 ${a + b > 26 ? 'May ' + (a + b - 26) : 'April ' + (4 + a + b)}\n`)
