70df1455e7bc5ad043cf385497b47847fc26bd90
Ferdinand Jason <2807-ferdinand.gondowijoyo@users.noreply.source.golabs.io>
2020-10-06 15:05:47 +0700

    Modify fibonacci to receive input dynamically

--- Maths_And_Stats/Number_Theory/Fibonacci_Number/Fibonacci_Number.clj
+++ Maths_And_Stats/Number_Theory/Fibonacci_Number/Fibonacci_Number.clj
@@ -3,6 +3,10 @@
   (map first (iterate 
              (fn [[a b]] [b (+ a b)]) [0 1])))
   
-(print (take 30 fibonacci))
+(defn generate-fibonacci
+  [n]
+  (take n fibonacci))
+
+(print (generate-fibonacci 30))
 ; will print 
 ; (0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765 10946 17711 28657 46368 75025 121393 196418 317811 514229)
