ae1c1d3693138edbc53a313b6aabdc6cae10acc1
Guilherme Oliveira Casagrande <guiolicasa@gmail.com>
2020-10-06 07:19:55 -0300

    Add => Math_And_Stats => Algebra => Markov_Matrix => JS

---
+++ Maths_And_Stats/Algebra/Markov_Matrix/Markov_Matrix.js
@@ -0,0 +1,57 @@
+function isMarkovMatrix(matrix) {
+    let isRight = isRightMarkov(matrix)
+    let isLeft = isLeftMarkov(matrix)
+    if (isRight && isLeft) { //If the matrix is a right and left Markov algorithm -> doubly markov matrix
+        return 3
+    } else if (isLeft) { //If the matrix is a left markov algorithm
+        return 2
+    } else if (isRight) { //If the matrix is a right markov algorithm
+        return 1
+    } else { //If every test fails, then its not a Markov matrix
+        return 0
+    }
+}
+
+function isRightMarkov(matrix) { //Each row summing to 1
+    if (
+        matrix
+            .map(val => { //Get each row of the matrix and transform it to the
+                //sum of all the elements in each row 
+                return val.reduce((prev, curr) => prev + curr)
+            })
+            .every(row => row == 1) //Check if every sum equals to 1
+    ) {
+        return true //If check is true
+    }
+    return false //If check is false
+}
+
+function isLeftMarkov(matrix) { //Each collumn summing to 1
+    let rot_matrix = matrix.map((val, index) => matrix.map(row => row[index])) //Rotates the matrix 90°
+
+    //When rotated, collumns become rows, so we can reuse the isRightMarkov function
+
+    return isRightMarkov(rot_matrix) //See if its a right markov matrix
+}
+
+//EXAMPLE INPUT
+let example_matrix = [
+    [0.9, 0.1],
+    [0.5, 0.5],
+]
+
+//EXAMPLE OUTPUT
+switch (isMarkovMatrix(example_matrix)) {
+    case 3:
+        console.log("doubly markov matrix")
+        break
+    case 2:
+        console.log("left markov matrix")
+        break
+    case 1:
+        console.log("right markov matrix")
+        break
+    case 0:
+        console.log("its not a markov matrix")
+        break
+}
