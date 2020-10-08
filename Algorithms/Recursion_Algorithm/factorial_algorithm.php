d4117ff37cde79bb085cdb191ccca4d7ed9df07d
Adaa <adaamgbede@gmail.com>
2020-10-08 22:39:19 +0100

    Solved Factorial Algorithm

--- Algorithms/Recursion_Algorithm/factorial_algorithm.php
+++ Algorithms/Recursion_Algorithm/factorial_algorithm.php
@@ -3,6 +3,6 @@
 function factorial($n) {
     if ($n < 0) return false;
     if ($n == 0 || $n == 1) return 1;
-    while($n > 1) return $n * factorial($n - 1);
+    while($n > 1) return ($n * factorial($n - 1));
 }
 echo factorial(0);
