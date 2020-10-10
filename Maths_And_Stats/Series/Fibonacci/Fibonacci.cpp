6026aac42cb32e0ba4eb21b8dc4b81d1755f0787
Krishna Kumar <57854052+Krishna3102@users.noreply.github.com>
2020-10-10 07:44:58 +0530

    Update Fibonacci.cpp

--- Maths_And_Stats/Series/Fibonacci/Fibonacci.cpp
+++ Maths_And_Stats/Series/Fibonacci/Fibonacci.cpp
@@ -1,23 +1,20 @@
-// Nothing Special
-
-#include <iostream>
-#include <cinttypes>
-
-int main() {
-    std::ios_base::sync_with_stdio(false) , std::cin.tie(nullptr);
-
-    int32_t t1 = 0 , t2 = 1 , next_term;
-    
-    int32_t n;  std::cin >> n;
-
-    puts("Fibonacci Series ...");
-
-    for (int32_t i = 0; i <= n; i++) {
-        std::cout << t1 << "\t";
-        next_term = t1 + t2;
-        t1 = t2;
-        t2 = next_term;
-    }
-    std::cout << std::endl;
-    return 0;
-}
+#include<bits/stdc++.h> 
+using namespace std; 
+  
+int fib(int n) 
+{ 
+    int a = 0, b = 1, c, i; 
+    if( n == 0) 
+        return a; 
+    for(i = 2; i <= n; i++) { 
+       c = a + b; 
+       a = b; 
+       b = c; 
+    } 
+    return b; 
+} 
+int main(){ 
+    int n = 9; 
+    cout << fib(n); 
+    return 0; 
+} 
