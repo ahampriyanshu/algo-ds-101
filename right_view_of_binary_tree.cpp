966ef430b1abe01bd93d39933fdde8340d5b3264
sanjiti0606 <44472481+sanjiti0606@users.noreply.github.com>
2020-10-11 12:34:54 +0530

    Update right_view_of_binary_tree.cpp

--- right_view_of_binary_tree.cpp
+++ right_view_of_binary_tree.cpp
@@ -1,18 +1,20 @@
 #include<bits/stdc++.h> 
 using namespace std;
-
-/**
- * Definition for binary tree
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
-*/
-
+//Definition for binary tree
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+};
+//Utility function to create Tree nodes
+TreeNode* newNode(int val) { 
+    TreeNode *temp = new TreeNode; 
+    temp->val = val; 
+    temp->left = temp->right = NULL; 
+    return temp; 
+} 
 //The task is equivalent to returning the last value in each level of the tree, ie. similar to Breadth First Traversal
-vector<int> rightView(TreeNode* root) {
+vector<int> getRightView(TreeNode* root) {
     vector<int>answer;
     queue<TreeNode*>Q;          
     TreeNode* temp;
@@ -37,3 +39,19 @@ vector<int> rightView(TreeNode* root) {
     }
     return answer;
 }
+int main() { 
+    //constructing the tree-
+    TreeNode* root = newNode(10); 
+    root->left = newNode(2); 
+    root->right = newNode(3); 
+    root->left->left = newNode(7); 
+    root->left->right = newNode(8); 
+    root->right->right = newNode(15); 
+    root->right->left = newNode(12); 
+    root->right->right->left = newNode(14); 
+    vector<int> RightViewOfBTree = getRightView(root);
+    //printing the vector containing right view of this tree-
+    for(int i=0; i<RightViewOfBTree.size(); i++)
+        cout<<RightViewOfBTree[i]<<" ";
+    cout<<"\n";
+} 
