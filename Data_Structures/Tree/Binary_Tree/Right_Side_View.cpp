0dc89cb284224f622b3fe484580521b3125aad2d
Adhiti <shaili.adhiti@gmail.com>
2020-10-11 19:57:02 +0530

    Right_Side_View

---
+++ Data_Structures/Tree/Binary_Tree/Right_Side_View.cpp
@@ -0,0 +1,141 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+vector<int> rightView(struct Node *root);
+
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+int main() {
+    int t;
+    string  tc;
+    getline(cin,tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* root = buildTree(s);
+
+        vector<int> vec = rightView(root);
+        for(int x : vec){
+            cout<<x<<" ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+
+vector<int> rightView(Node *root)
+{
+   vector<int> v;
+   if(root==NULL)
+   {
+       return v;
+   }
+   
+   queue<Node*> q;
+   q.push(root);
+   while(!q.empty())
+   {
+       int size=q.size();
+       
+       while(size>0)
+       {
+           Node* temp=q.front();
+           q.pop();
+           if(size==1)
+           {
+               v.push_back(temp->data);
+           }
+           if(temp->left!=NULL)
+           {
+               q.push(temp->left);
+           }
+           if(temp->right!=NULL)
+           {
+               q.push(temp->right);
+           }
+           size--;
+           
+       }
+   }
+   return v;
+}
