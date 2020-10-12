62a42deecb4f2a8c025370ce1778b0ca6450062a
Sanchi7Bhalla <63395366+Sanchi7Bhalla@users.noreply.github.com>
2020-10-12 15:44:37 +0530

    Update Brick_wall.cpp

--- Data_Structures/Maps_Hash_Dict/Brick_Wall/Brick_wall.cpp
+++ Data_Structures/Maps_Hash_Dict/Brick_Wall/Brick_wall.cpp
@@ -1,23 +1,25 @@
-class Solution
-{
+class Solution {
 public:
-    int leastBricks(vector<vector<int>> &wall)
-    {
-        unordered_map<int, int> m;
-        //Maintain a sort of prefix array with counts for each sum
-        int max_surpass = 0;
-        int res = wall.size();
-        int sum;
-        for (int i = 0; i < wall.size(); i++)
+    int leastBricks(vector<vector<int>>& wall) {
+        unordered_map<int,int> m;
+        int max=INT_MIN;
+        for(int i=0;i<wall.size();i++)
         {
-            sum = 0;
-            for (int j = 0; j < wall[i].size() - 1; j++)
+            int curr=0;
+            for(int j=0;j<wall[i].size()-1;j++)
             {
-                sum += wall[i][j];
-                m[sum] += 1;
-                max_surpass = max(max_surpass, m[sum]);
+                curr=curr+wall[i][j];
+                if(m.find(curr)==m.end())
+                    m[curr]=1;
+                else
+                    m[curr]++;
+                if(max<m[curr])
+                    max=m[curr];
             }
         }
-        return wall.size() - max_surpass;
+       
+        if(max==INT_MIN)
+            return wall.size();
+        return wall.size()-max;
     }
 };
