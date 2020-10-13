148a72488c59f0288a682e1ee4ab9242878520d6
aroradhruv2308 <aroradhruv2308@gmail.com>
2020-10-13 09:57:08 +0530

    LCA|Python

---
+++ Algorithms/Tree_Algorithms/LCA/LCA.py
@@ -0,0 +1,21 @@
+# Leetcode Problem Link:
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root == None or root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left,p,q)
+        right = self.lowestCommonAncestor(root.right,p,q)
+        if left == None:
+            return right
+        if right == None:
+            return left
+        return root
