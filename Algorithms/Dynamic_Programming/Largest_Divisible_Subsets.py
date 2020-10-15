5ac7fb508f7b40fc59e32f10dd0538accd91e168
Lakshitha Wisumperuma <swisum@gmail.com>
2020-10-15 10:04:16 +0530

    Update to add user's input dynamically.

--- Algorithms/Dynamic_Programming/Largest_Divisible_Subsets.py
+++ Algorithms/Dynamic_Programming/Largest_Divisible_Subsets.py
@@ -3,7 +3,14 @@ def largestDivisibleSubset(nums):
     for x in sorted(nums):
         S[x] = max((S[d] for d in S if x % d == 0), key=len) | {x}
     return list(max(S.values(), key=len))
-    
+
 if __name__ == '__main__': 
-    arr = [1, 2, 17, 4] 
-    print(largestDivisibleSubset(arr))
+
+    lst = [] 
+    n = int(input("Enter number of elements : ")) 
+    for i in range (n):
+        x=int(input("enter no. \n")) 
+        lst.insert(i,x)
+        i+=1
+
+    print(largestDivisibleSubset(lst))
