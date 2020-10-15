f305fc7281dec23b1fd36881d085a9dec1c20736
Harsh-1906 <68503473+Harsh-1906@users.noreply.github.com>
2020-10-15 09:57:54 +0530

    Added => Data Structure => Linked List => Singly Linked List => Palindrome Linked List => Java

---
+++ Data_Structures/Linked_List/Singly_Linked_List/Java/PalindromeLinkedList.java
@@ -0,0 +1,44 @@
+
+
+ public class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+    ListNode fast = head, slow = head;
+    while (fast != null && fast.next != null) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    if (fast != null) { // odd nodes: let right half smaller
+        slow = slow.next;
+    }
+    slow = reverse(slow);
+    fast = head;
+    
+    while (slow != null) {
+        if (fast.val != slow.val) {
+            return false;
+        }
+        fast = fast.next;
+        slow = slow.next;
+    }
+    return true;
+}
+
+public ListNode reverse(ListNode head) {
+    ListNode prev = null;
+    while (head != null) {
+        ListNode next = head.next;
+        head.next = prev;
+        prev = head;
+        head = next;
+    }
+    return prev;
+}
+}
