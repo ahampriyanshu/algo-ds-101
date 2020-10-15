0cc1c7dc993136892e4101f02e813adde66b3f3d
AtalKhandelwal <54740592+AtalKhandelwal@users.noreply.github.com>
2020-10-15 11:53:17 +0530

    Update ExponentialSearch.cs

--- Algorithms/Search_Algorithms/Exponential_Search/ExponentialSearch.cs
+++ Algorithms/Search_Algorithms/Exponential_Search/ExponentialSearch.cs
@@ -37,8 +37,8 @@ static int binarySearch(int []arr, int left, int right, int search)
 
 public static void Main() 
 { 
-	int []arr = {5,8,13,51,143,200}; 
-	int search = 10; 
+	int []arr = {5,8,13,51,143,200,254}; 
+	int search = 100; 
 	int result = exponentialSearch(arr, arr.Length, search); 
 	if (result == -1){ 
 		Console.Write("Element is not present in array"); 
