ec254277a1392a77c693f452ed202fd8d85a7275
Nathan Pickard <nathan.pickard87@gmail.com>
2020-10-17 10:05:16 -0700

    Added dynamic user input

--- Maths_And_Stats/Algebra/Babylonian/Babylonian.js
+++ Maths_And_Stats/Algebra/Babylonian/Babylonian.js
@@ -1,18 +1,22 @@
-function sqrt(n) {
+function babylonianSqrt(n) {
+
+  // Checks for valid number input
   if (!(typeof n === 'number' && n >= 0 && !isNaN(n))) {
     return NaN;
   } else if (n === 0) {
     return 0;
   } else if (n === Infinity) {
     return Infinity;
-
   }
 
   var value = n;
 
   while (true) {
     var last = value;
+
+    // Calculate value
     value = (value + n / value) * 0.5;
+    // Check if true
     if (Math.abs(value - last) < 1e-9) {
       break;
     }
@@ -20,3 +24,8 @@ function sqrt(n) {
 
   return value;
 }
+
+// Test
+let userInput = parseInt(prompt("Enter a number to find the Babylonian square root:"))
+
+babylonianSqrt(userInput);
