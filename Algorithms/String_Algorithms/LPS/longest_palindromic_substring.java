76a5bc0afeeaa4df76b3dac45df27916d7e4a771
harshitashankar <68508399+harshitashankar@users.noreply.github.com>
2020-10-19 13:47:27 +0530

    Adfding comments

--- Algorithms/String_Algorithms/LPS/longest_palindromic_substring.java
+++ Algorithms/String_Algorithms/LPS/longest_palindromic_substring.java
@@ -1,9 +1,13 @@
+//using dynamic programming approach to solve
+
 class Solution {
     public static String longestPalindrome(String s) {
         int n=s.length();
-        int[][] memory = new int[n][n];
-        int maxLen=1;
-        int start=0;
+        
+        //2-d array to store the palindrome status of each substring -> memory[i][j]=1 iff substring s(i,j) is palindrome otheriwise memory[i][j]=0
+        int[][] memory = new int[n][n]; 
+        int maxLen=1; //maxLen stores current maximum length of palindrome substring
+        int start=0; //start stores the start index of the current longest palindrome string
 
         for(int i=n-1;i>=0;i--)
         {
@@ -12,12 +16,12 @@ class Solution {
 
                 if(i==j)
                 {
-                    memory[i][j]=1;
+                    memory[i][j]=1; //updating memory for substrings of length 1. 
                     continue;
                 }
                 if(j==i+1)
                 {
-                    memory[i][i+1]=s.charAt(j)==s.charAt(i)?1:0;
+                    memory[i][i+1]=s.charAt(j)==s.charAt(i)?1:0; //updating memory with substrings of length 2
                     if (memory[i][i+1]==1 && maxLen<=2)
                     {
                         maxLen=2;
@@ -26,8 +30,8 @@ class Solution {
                     continue;
                 }   
 
-
-                if(memory[i+1][j-1]==1 && s.charAt(j)==s.charAt(i))
+                //for each substring s(i,j) of lenght>2, it is a palindrome if substring s(i+1,j-1) is a palindrome and s(i)==s(j)
+                if(memory[i+1][j-1]==1 && s.charAt(j)==s.charAt(i)) 
                 {
                     memory[i][j]=1;
                     int len = j-i+1;
@@ -41,11 +45,11 @@ class Solution {
 
             }
         }
-        return s.substring(start,start+maxLen);
+        return s.substring(start,start+maxLen); //returning longest palindrome substring
     }
     public static void main(String[] args)
     {
-           System.out.println(longestPalindrome("abbaca"));
+           System.out.println(longestPalindrome("abbaca")); //alter statement acc to preferred input
     }
 }
     
