28f1682ce53a2ad827f40d4beb00d1f16ffbd937
Tushar Diwakar <44468541+Tusharsd123@users.noreply.github.com>
2021-10-01 20:22:25 +0530

    Added=>Algo=>Array=>CPP

---
+++ Data-Structures/Array/Array.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	//number of elements as input
+	cin >> n;
+	int arr[n]; //initiallize array
+	for(int i =0;i<n;i++)
+	{
+		cin >> arr[i]; // input the elements
+	}
+	for(int i=0;i<n;i++)
+	{
+		cout << arr[i] << " ";//traverse the array and print the elements
+	}
+	return 0;
+}
