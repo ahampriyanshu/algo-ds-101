58152889976a7c84d905b4ac1a3ab1712ebe3946
dhruvt <dhruvtalan2436@gmail.com>
2021-10-03 01:38:43 +0530

    Unordered Map

---
+++ STL/unorderedmap/unorderedmap.cpp
@@ -0,0 +1,22 @@
+// C++ program to demonstrate functionality of unordered_map
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main()
+{
+	// Declaring umap to be of <string, int> type
+	// key will be of string type and mapped value will
+	// be of int type
+	unordered_map<string, int> umap;
+
+	// inserting values by using [] operator
+	umap["GeeksforGeeks"] = 10;
+	umap["Practice"] = 20;
+	umap["Contribute"] = 30;
+
+	// Traversing an unordered map
+	for (auto x : umap)
+	cout << x.first << " " << x.second << endl;
+
+}
