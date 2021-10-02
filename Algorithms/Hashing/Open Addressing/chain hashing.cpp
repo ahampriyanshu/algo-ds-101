69c76ae4f3f76f7579d2b465423298f97deddab7
unknown <bharanisrianbu@gmail.com>
2021-10-03 01:09:07 +0530

    chain hashing

---
+++ Algorithms/Hashing/Open Addressing/chain hashing.cpp
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define size 7
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *chain[size];
+
+void init()
+{
+    int i;
+    for(i = 0; i < size; i++)
+        chain[i] = NULL;
+}
+
+void insert(int value)
+{
+    //create a newnode with value
+    struct node *newNode ;
+    newNode= (node*)malloc(sizeof(struct node));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    // hash key
+    int key = value % size;
+
+    //check if chain[key] is empty
+    if(chain[key] == NULL)
+        chain[key] = newNode;
+    //collision
+    else
+    {
+        //add the node at the end of chain[key].
+        struct node *temp = chain[key];
+        while(temp->next)
+        {
+            temp = temp->next;
+        }
+
+        temp->next = newNode;
+    }
+}
+
+void print()
+{
+    int i;
+
+    for(i = 0; i < size; i++)
+    {
+        struct node *temp = chain[i];
+        printf("chain[%d]-->",i);
+        while(temp)
+        {
+            printf("%d -->",temp->data);
+            temp = temp->next;
+        }
+        printf("NULL\n");
+    }
+}
+
+int main()
+{
+    //init array of list to NULL
+    init();
+
+    insert(7);
+    insert(0);
+    insert(3);
+    insert(10);
+    insert(4);
+    insert(5);
+
+    print();
+
+    return 0;
+}
