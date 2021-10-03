4d8969867e1189a203663ec2014d936e7bfb13da
Subham Mohanty <65271698+subham5230@users.noreply.github.com>
2021-10-03 17:31:10 +0530

    Added => Algorithms => Tree-Algorithm => Sorted_Array_To_BST => SortedArrayToBST.java

--- Algorithms/Tree-Algorithm/Sorted_Array_To_BST/SortedArrayToBST.java
+++ Algorithms/Tree-Algorithm/Sorted_Array_To_BST/SortedArrayToBST.java
@@ -46,19 +46,58 @@ class BinaryTree {
 public class SortedArrayToBST {
 
 	public static void main(String[] args) {
-        BinaryTree tree = new BinaryTree();
-        int arr[] = new int[]{10, 20, 30, 40, 50, 60, 70};
-        int n = arr.length;
-        Node root = tree.sortedArrayToBST(arr, 0, n - 1);
-        
-        System.out.println("Preorder traversal of generated BST");
-        tree.preOrder(root);
+		
+		Scanner sc = new Scanner(System.in);
+
+			BinaryTree tree = new BinaryTree();
+
+			System.out.println("Please Enter the Size of the Array:");
+			int n = sc.nextInt();
+
+		int arr[] = new int[n];
+
+		for(int i=0; i<n; i++) {
+			System.out.println("Enter Array element " + (i+1) + ": ");
+			arr[i] = sc.nextInt();
+		}
+
+		System.out.println("\nProcessing Array....");
+		Arrays.sort(arr);
+		System.out.printf("Sorted Array -> %s \n", Arrays.toString(arr));
+
+		System.out.println("\nGenerating BST from Sorted Array....");
+		Node root = tree.sortedArrayToBST(arr, 0, n - 1);
+
+		System.out.println("\nPreorder traversal of generated BST");
+		tree.preOrder(root);
     }
 }
 
 /* 
- * Output:
- * 			Preorder traversal of generated BST
- *			40 20 10 30 60 50 70 
+ * OUTPUT=>
+ * 
+ *  Please Enter the Size of the Array:
+ *	6
+ *	Enter Array element 1: 
+ *	46
+ *	Enter Array element 2: 
+ *	88
+ *	Enter Array element 3: 
+ *	15
+ *	Enter Array element 4: 
+ *	76
+ *	Enter Array element 5: 
+ *	31
+ *	Enter Array element 6: 
+ *	62
+ *	
+ *	Processing Array....
+ *	Sorted Array -> [15, 31, 46, 62, 76, 88] 
+ *	
+ *	Generating BST from Sorted Array....
+ *	
+ *	Preorder traversal of generated BST
+ *	46 15 31 76 62 88  
  * 
  */
+
