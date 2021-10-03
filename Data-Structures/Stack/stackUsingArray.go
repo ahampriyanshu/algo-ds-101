931a2d6fbfa93020e0fbccde6d55cf8b0697be4f
harshitsuthar77731 <65647274+harshitsuthar77731@users.noreply.github.com>
2021-10-03 11:59:54 +0530

    stack using array in go

--- Data-Structures/Stack/stackUsingArray.go
+++ Data-Structures/Stack/stackUsingArray.go
@@ -1,41 +1,32 @@
-type stack struct {
-     lock sync.Mutex
-     s []int
+package main
+import (
+	"fmt"
+)
+
+type stack []int
+
+func (s stack) push(v int) stack {
+    return append(s, v)
 }
 
-func NewStack() *stack {
-    return &stack {sync.Mutex{}, make([]int,0), }
+func (s stack) pop() (stack, int) {
+    l := len(s)
+   { return  s[:l-1], s[l-1]}
 }
 
-func (s *stack) Push(v int) {
-    s.lock.Lock()
-    defer s.lock.Unlock()
-
-    s.s = append(s.s, v)
+func (s stack) size() int {
+    return len(s)
 }
-
-func (s *stack) Pop() (int, error) {
-    s.lock.Lock()
-    defer s.lock.Unlock()
-
-
-    l := len(s.s)
-    if l == 0 {
-        return 0, errors.New("Empty Stack")
-    }
-
-    res := s.s[l-1]
-    s.s = s.s[:l-1]
-    return res, nil
-}
-
 
 func main(){
-    s := NewStack()
-    s.Push(1)
-    s.Push(2)
-    s.Push(3)
-    fmt.Println(s.Pop())
-    fmt.Println(s.Pop())
-    fmt.Println(s.Pop())
+    s := make(stack,0)
+    s = s.push(101)
+    s = s.push(203)
+    s = s.push(399)
+    s = s.push(501)
+    s, p := s.pop()
+    fmt.Println(p)
+    fmt.Println( s.size())
+	
+
 }
