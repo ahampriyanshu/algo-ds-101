fdb94abb5d6862f2331033408e815eb105236e50
MuhammadFajrian <muhammadfajrianpp@gmail.com>
2021-10-04 21:55:35 +0700

    Modified => Algorithms => Searching => Binary_Search => BinarySearch.go

--- Algorithms/Searching/Binary_Search/BinarySearch.go
+++ Algorithms/Searching/Binary_Search/BinarySearch.go
@@ -1,10 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	binarySearch := BinarySearch(arr, 5)
+	fmt.Print("Enter the number you want to search : ")
+	var input int
+
+	//Taking input from user
+	fmt.Scanln(&input)
+
+	binarySearch := BinarySearch(arr, input)
 	fmt.Println(binarySearch)
 }
 
@@ -14,9 +22,11 @@ func BinarySearch(arr []int, x int) string {
 		end   = len(arr) - 1
 		mid   = -1
 	)
-
+	//start looping for searching index
 	for start != mid && end != mid {
+		//set mid index
 		mid = start + (end-start)/2
+		//check if input equal at index mid then return
 		if arr[mid] == x {
 			return fmt.Sprintf("Selection found at index %d", mid)
 		}
