c84a17a3fcdb4c05d9136f54fb4a0e7fa5c436d9
Nishit Bansal <nishitbnsl@gmail.com>
2021-10-04 17:17:23 +0530

    Added TrappingWater.cpp issue #1809

---
+++ Algorithms/Array-Algorithm/TrappingWater.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int trap(vector<int>& h) {
+        int n = h.size();
+        int left =  0;
+        int maxLeft = 0, maxRight = 0;
+        int right = n-1;
+        int ans = 0;
+        
+        while(left <= right){
+            if(h[left] <= h[right]){
+                if(maxLeft <= h[left]) maxLeft = h[left];
+                else ans+=maxLeft-h[left];
+                left++;
+            } else {
+                if(maxRight <= h[right]) maxRight = h[right];
+                else ans+=maxRight - h[right];
+                right--;
+            }
+        }
+        return ans;
+    }
+int main(){
+    int n;
+    cin >> n;
+    vector <int> v(n);
+    int x;
+    for(int i=0;i<n;i++){
+        cin>>x;
+        v[i] = x;
+    }
+    
+    cout << trap(v) << endl;
+}
