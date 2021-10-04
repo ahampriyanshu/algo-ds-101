a9ff2eb69f89ecbb870b454d66746d6e6283fe06
Tanushreesuthar <76775979+Tanushreesuthar@users.noreply.github.com>
2021-10-04 20:52:01 +0530

    Create Josephus.cs

---
+++ Algorithms/Recursion/Josephus.cs
@@ -0,0 +1,24 @@
+using System;
+
+class TEMP {
+
+	static int josephus(int n, int k)
+	{
+		if (n == 1)
+			return 1;
+		else
+			
+			return (josephus(n - 1, k) + k - 1) % n + 1;
+	}
+
+	
+	public static void Main()
+	{
+		int n ;
+		int k ;
+        n= Convert.ToInt32(Console.ReadLine());
+        k= Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("The chosen "
+                          + "place is " + josephus(n, k));
+	}
+}
