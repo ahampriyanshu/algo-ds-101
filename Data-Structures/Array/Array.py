87e38c05fd012d64ba938ecf484226ed0e8b0b23
Magnum-07 <66016411+Magnum-07@users.noreply.github.com>
2021-10-04 14:45:43 +0530

    Kadane's Algorithm Update
    I added an algorithm which is used to find the maximum sum in an array.

--- Data-Structures/Array/Array.py
+++ Data-Structures/Array/Array.py
@@ -1,8 +1,12 @@
-n = int(input("Enter number of elements : "))
-arr=[]
-for i in range(0,n):
-    a = int(input())
+#  Kadanes algorithm
+#  BY this algorithm we find the maximum sum that can come from an array
+ls = [-2, 1, 0, 5, -3]
+max_so_far = 0
+max_till_here = 0
+for i in range(len(ls)):
+    max_so_far = ls[i]
+    if max_so_far <= 0:
+        max_so_far = 0
+    max_till_here += max_so_far
 
-    arr.append(a)
-
-print(arr)
+print(max_till_here)
