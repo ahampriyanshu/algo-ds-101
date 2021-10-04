bd949e616ecf65888ec7f89eac1aad9f0cd00dcd
Nikitha Rangineni <63146359+nikku971@users.noreply.github.com>
2021-10-04 11:15:25 +0530

    Update GenericTree.cpp

--- Data-Structures/Generic Tree/GenericTree.cpp
+++ Data-Structures/Generic Tree/GenericTree.cpp
@@ -7,33 +7,51 @@ using namespace std;
 struct Node
 {
     int key;
-    vector<Node *>child;
+    vector<Node*> children;//reference to child nodes
+    Node(int x)
+   {
+      key=x;
+   }
 };
-Node* newNode(int x)
-{
-     Node* k=new Node();
-     k->key=x;
-     return k;
-}
+
 int main()
 {
-  ios_base::sync_with_stdio(0); cin.tie(0);
-  #ifndef ONLINE_JUDGE
-       freopen("input.txt", "r", stdin);
-         freopen("outpu.txt", "w", stdout);
-    #endif 
+  ios_base::sync_with_stdio(0); cin.tie(0); //for fast IO
+       Node *root = new Node(10);
+       int n;
+       //enter n numbers that will be children of root
+       cin>>n;
+       for(int i=0;i<n;i++)
+       {
+           int x;
+           cin>>x;
+           root->children.push_back(new Node(x));
+       }
+       //enter x1 numbers that will children of first child
+       int x1;
+       cin>>x1;
+       for(int i=0;i<x1;i++)
+       {
 
-       Node *root = newNode(10);
-    (root->child).push_back(newNode(2));
-    (root->child).push_back(newNode(34));
-    (root->child).push_back(newNode(56));
-    (root->child).push_back(newNode(100));
-    (root->child[0]->child).push_back(newNode(77));
-    (root->child[0]->child).push_back(newNode(88));
-    (root->child[2]->child).push_back(newNode(1));
-    (root->child[3]->child).push_back(newNode(7));
-    (root->child[3]->child).push_back(newNode(8));
-    (root->child[3]->child).push_back(newNode(9));
-    //cout<<root->key;
+           int x;
+           cin>>x;
+           Node* k=new Node(x);
+           root->children[0]->children.push_back(k);
+
+       }
+       int x2;
+       cin>>x2;
+        //enter x2 numbers that will children of first child
+       for(int i=0;i<x2;i++)
+       {
+            
+           int x;
+           cin>>x;
+           root->children[1]->children.push_back(new Node(x));
+
+       }
+      // dynamically we can more children 
+   
+       cout<<root->children[1]->key;
 
        }
